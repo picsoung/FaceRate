f5f56f1ff7424ebe6d41ac338187848edbe68d33
plunchete <plunchete@gmail.com>
2011-08-13 20:30:15 -0700

    First working version integrated with embed.ly

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,37 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import java.util.*;
+import org.apache.commons.io.IOUtils;
 
-import models.*;
+import play.Logger;
+import play.mvc.Controller;
+import siena.Json;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+    
+    public static void getLinkHTML(String url) throws MalformedURLException, IOException {
+    	Logger.info("Trying to get the content for " + url);
+    	String apiKey = "bcd380f0c61e11e0b9a74040d3dc5c07";
+    	String baseUrl = "http://api.embed.ly/1/oembed?key="+apiKey +"&url="+url;//api.embed.ly/1/oembed?key=:key&url=:url&maxwidth=:maxwidth&maxheight=:maxheight&format=:format&callback=:callback
+    	Logger.info("Requesting for " + baseUrl);
+    	URL u = new URL(baseUrl);
+		HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+    	//HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+    	InputStream in = conn.getInputStream();
+		byte[] bytes = IOUtils.toByteArray(in);
+		String stringResponse = new String(bytes);
+		Logger.info("Response " + stringResponse);
+		Json json =  Json.loads(stringResponse);
+		renderJSON(json.get("html").toString());
     }
 
 }
