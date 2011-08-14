71a03d2a960013668410cdcef29aaa09e6e08bc0
Nicolas Grenie <nicolas.grenie@gmail.com>
2011-08-14 10:01:43 -0700

    Conf file created

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import org.apache.commons.io.IOUtils;
 
 import play.Logger;
+import play.Play;
 import play.mvc.Controller;
 import siena.Json;
 
@@ -20,7 +21,7 @@ public class Application extends Controller {
     
     public static void getLinkHTML(String url) throws MalformedURLException, IOException {
     	Logger.info("Trying to get the content for " + url);
-    	String apiKey = "bcd380f0c61e11e0b9a74040d3dc5c07";
+    	String apiKey = (String) Play.configuration.get("embedly.apikey");
     	String baseUrl = "http://api.embed.ly/1/oembed?key="+apiKey +"&url="+url;//api.embed.ly/1/oembed?key=:key&url=:url&maxwidth=:maxwidth&maxheight=:maxheight&format=:format&callback=:callback
     	Logger.info("Requesting for " + baseUrl);
     	URL u = new URL(baseUrl);
